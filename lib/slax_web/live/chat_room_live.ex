5337f633638e20caab940100e20e6e713de729a6
cornelia <corneliakelinske@gmail.com>
2024-10-08 15:28:53 -0700

    add messages

--- lib/slax_web/live/chat_room_live.ex
+++ lib/slax_web/live/chat_room_live.ex
@@ -1,7 +1,7 @@
 defmodule SlaxWeb.ChatRoomLive do
   use SlaxWeb, :live_view
   alias Slax.Chat
-  alias Slax.Chat.Room
+  alias Slax.Chat.{Message, Room}
 
   def render(assigns) do
     ~H"""
@@ -85,6 +85,9 @@ defmodule SlaxWeb.ChatRoomLive do
       <% end %>
     </ul>
       </div>
+      <div class="flex flex-col flex-grow overflow-auto">
+       <.message :for={message <- @messages} message={message} />
+     </div>
     </div>
     """
   end
@@ -109,6 +112,24 @@ defmodule SlaxWeb.ChatRoomLive do
     """
   end
 
+  attr :message, Message, required: true
+
+  defp message(assigns) do
+    ~H"""
+    <div class="relative flex px-4 py-3">
+      <div class="h-10 w-10 rounded flex-shrink-0 bg-slate-300"></div>
+      <div class="ml-2">
+        <div class="-mt-1">
+          <.link class="text-sm font-semibold hover:underline">
+            <span>User</span>
+          </.link>
+          <p class="text-sm"><%= @message.body %></p>
+        </div>
+      </div>
+    </div>
+    """
+  end
+
   def mount(_params, _session, socket) do
     rooms = Chat.list_rooms()
 
@@ -125,7 +146,16 @@ defmodule SlaxWeb.ChatRoomLive do
           Chat.get_first_room!()
       end
 
-    {:noreply, assign(socket, hide_topic?: false, page_title: "#" <> room.name, room: room)}
+      messages = Chat.list_messages_in_room(room)
+
+      {:noreply,
+       assign(socket,
+         hide_topic?: false,
+         messages: messages,
+         page_title: "#" <> room.name,
+         room: room
+       )}
+
   end
 
   def handle_event("toggle-topic", _params, socket) do
