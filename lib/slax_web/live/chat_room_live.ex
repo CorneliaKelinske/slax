e3c731820ae75b04e643783b4e336f46e3715431
cornelia <corneliakelinske@gmail.com>
2025-01-23 12:34:39 -0800

    thread component

--- lib/slax_web/live/chat_room_live.ex
+++ lib/slax_web/live/chat_room_live.ex
@@ -1,10 +1,12 @@
 defmodule SlaxWeb.ChatRoomLive do
   use SlaxWeb, :live_view
+  import SlaxWeb.ChatComponents
   import SlaxWeb.UserComponents
   alias Slax.Accounts
   alias Slax.Accounts.User
   alias Slax.Chat
   alias Slax.Chat.{Message, Room}
+  alias SlaxWeb.ChatRoomLive.ThreadComponent
   alias SlaxWeb.OnlineUsers
 
   def render(assigns) do
@@ -242,6 +244,17 @@ defmodule SlaxWeb.ChatRoomLive do
         current_user={@current_user}
       />
     <% end %>
+    <%= if assigns[:thread] do %>
+      <.live_component
+        id="thread"
+        module={ThreadComponent}
+        message={@thread}
+        room={@room}
+        current_user={@current_user}
+        timezone={@timezone}
+      />
+    <% end %>
+
     <.modal
       id="new-room-modal"
       show={@live_action == :new}
@@ -331,50 +344,6 @@ defmodule SlaxWeb.ChatRoomLive do
         {@room.name}
       </span>
     </.link>
-    """
-  end
-
-  attr :current_user, User, required: true
-  attr :dom_id, :string, required: true
-  attr :message, Message, required: true
-  attr :timezone, :string, required: true
-
-  defp message(assigns) do
-    ~H"""
-    <div id={@dom_id} class="group relative flex px-4 py-3">
-      <button
-        :if={@current_user.id == @message.user_id}
-        class="absolute top-4 right-4 text-red-500 hover:text-red-800 cursor-pointer
-        hidden group-hover:block"
-        data-confirm="Are you sure?"
-        phx-click="delete-message"
-        phx-value-id={@message.id}
-      >
-        <.icon name="hero-trash" class="h-4 w-4" />
-      </button>
-
-      <.user_avatar
-        user={@message.user}
-        class="h-10 w-10 rounded cursor-pointer"
-        phx-click="show-profile"
-        phx-value-user-id={@message.user.id}
-      />
-      <div class="ml-2">
-        <div class="-mt-1">
-          <.link
-            phx-click="show-profile"
-            phx-value-user-id={@message.user.id}
-            class="text-sm font-semibold hover:underline"
-          >
-            {@message.user.username}
-            <span :if={@timezone} class="ml-1 text-xs text-gray-500">
-              {message_timestamp(@message, @timezone)}
-            </span>
-          </.link>
-          <p class="text-sm">{@message.body}</p>
-        </div>
-      </div>
-    </div>
     """
   end
 
@@ -481,7 +450,7 @@ defmodule SlaxWeb.ChatRoomLive do
 
   def handle_event("show-profile", %{"user-id" => user_id}, socket) do
     user = Accounts.get_user!(user_id)
-    {:noreply, assign(socket, :profile, user)}
+    {:noreply, assign(socket, profile: user, thread: nil)}
   end
 
   def handle_event("close-profile", _, socket) do
@@ -537,12 +506,14 @@ defmodule SlaxWeb.ChatRoomLive do
     {:noreply, socket}
   end
 
-  def handle_info({:updated_avatar, user}, socket) do
-    socket
-    |> maybe_update_profile(user)
-    |> maybe_update_current_user(user)
-    |> push_event("update_avatar", %{user_id: user.id, avatar_path: user.avatar_path})
-    |> noreply()
+  def handle_event("close-thread", _, socket) do
+    {:noreply, assign(socket, :thread, nil)}
+  end
+
+  def handle_event("show-thread", %{"id" => message_id}, socket) do
+    message = Chat.get_message!(message_id)
+
+    socket |> assign(profile: nil, thread: message) |> noreply()
   end
 
   defp maybe_update_current_user(socket, user) do
@@ -561,6 +532,13 @@ defmodule SlaxWeb.ChatRoomLive do
     end
   end
 
+  def handle_info({:updated_avatar, user}, socket) do
+    socket
+    |> maybe_update_profile(user)
+    |> maybe_update_current_user(user)
+    |> push_event("update_avatar", %{user_id: user.id, avatar_path: user.avatar_path})
+    |> noreply()
+  end
 
   def handle_info({:new_message, message}, socket) do
     room = socket.assigns.room
@@ -601,12 +579,6 @@ defmodule SlaxWeb.ChatRoomLive do
 
   defp assign_message_form(socket, changeset) do
     assign(socket, :new_message_form, to_form(changeset))
-  end
-
-  defp message_timestamp(message, timezone) do
-    message.inserted_at
-    |> Timex.Timezone.convert(timezone)
-    |> Timex.format!("%-l:%M %p", :strftime)
   end
 
   defp maybe_insert_unread_marker(messages, nil), do: messages
